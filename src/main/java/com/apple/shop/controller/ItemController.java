7995afd34759a1ea1511a9f1f29a2bd82ce9f2fe
CHAEYEON KIM <gimchaeyeon57@gmail.com>
2024-08-08 15:59:34 +0900

    update code

--- src/main/java/com/apple/shop/controller/ItemController.java
+++ src/main/java/com/apple/shop/controller/ItemController.java
@@ -3,11 +3,16 @@ package com.apple.shop.controller;
 import com.apple.shop.domain.Item;
 import com.apple.shop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,8 +24,32 @@ public class ItemController {
     String list(Model model){
         List<Item> result = itemRepository.findAll();
         model.addAttribute("items",result);
-        var a = new Item();
-        System.out.println(a.toString());
         return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write(){
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(@RequestParam String title, @RequestParam Integer price) {
+        Item item = new Item();
+        item.setTitle(title);
+        item.setPrice(price);
+        itemRepository.save(item);//테이블에 하나의 행으로 저장해줄 것.
+        return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id,Model model) throws Exception{
+        throw new Exception();
+            /*Optional<Item> reult = itemRepository.findById(id);
+            if (reult.isPresent()) {
+                model.addAttribute("data", reult.get());
+                return "detail.html";
+            } else {
+                return "redirect:/list";
+            }*/
     }
 }
--- src/main/java/com/apple/shop/domain/Item.java
+++ src/main/java/com/apple/shop/domain/Item.java
@@ -13,9 +13,7 @@ public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String title;
-
     private Integer price;
 
 }
